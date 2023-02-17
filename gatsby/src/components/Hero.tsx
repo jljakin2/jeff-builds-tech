317990865fb538ea949cc04c392ccb0fdf6270e4
jljakin2 <jeff.jak13@gmail.com>
2023-02-16 21:51:29 -0500

    added typography defaults

--- gatsby/src/components/Hero.tsx
+++ gatsby/src/components/Hero.tsx
@@ -6,39 +6,53 @@ import { NormalRichText } from "./NormalRichText";
 import { GatsbyImage } from "gatsby-plugin-image";
 import { Link } from "gatsby";
 
-const HeroStyles = styled.section``;
+const HeroStyles = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
 
 export default function Hero({ data }: any) {
   const [linkedin, github, twitter] = data.socialLinks;
 
   return (
     <HeroStyles>
-      <a href={github.link} className="icon-link">
-        <Icon name={github.name} size="24" />
-      </a>
-      <a href={linkedin.link} className="icon-link">
-        <Icon name={linkedin.name} size="24" />
-      </a>
-      <a href={twitter.link} className="icon-link">
-        <Icon name={twitter.name} size="24" />
-      </a>
-      <h4>{data.greeting}</h4>
-      <PortableText
-        value={data._rawBody}
-        // @ts-ignore
-        components={NormalRichText}
-      />
-      <Link to="/blog" className="outline-btn">
-        Go To Blog
-      </Link>
-      <Link to="/projects" className="filled-btn">
-        Go To Portfolio
-      </Link>
+      <div className="social-links">
+        <a href={github.link} className="icon-link">
+          <Icon name={github.name} size="24" />
+        </a>
+        <a href={linkedin.link} className="icon-link">
+          <Icon name={linkedin.name} size="24" />
+        </a>
+        <a href={twitter.link} className="icon-link">
+          <Icon name={twitter.name} size="24" />
+        </a>
+      </div>
 
-      <GatsbyImage
-        image={data.image.asset.gatsbyImageData}
-        alt="Jeff working at his desk on his laptop"
-      />
+      <div className="text-container">
+        <h4>{data.greeting}</h4>
+        <PortableText
+          value={data._rawBody}
+          // @ts-ignore
+          components={NormalRichText}
+        />
+      </div>
+
+      <div className="button-container">
+        <Link to="/blog" className="outline-btn">
+          Go To Blog
+        </Link>
+        <Link to="/projects" className="filled-btn">
+          Go To Portfolio
+        </Link>
+      </div>
+
+      <div className="hero-container">
+        <GatsbyImage
+          image={data.image.asset.gatsbyImageData}
+          alt="Jeff working at his desk on his laptop"
+        />
+      </div>
     </HeroStyles>
   );
 }
--- gatsby/src/styles/Typography.tsx
+++ gatsby/src/styles/Typography.tsx
@@ -1,6 +1,54 @@
 import { createGlobalStyle } from "styled-components";
 
 export const Typography = createGlobalStyle`
+    h1 {
+        font-size: 3.5rem;
+        font-weight: 700;
+        line-height: 3.625rem;
+        letter-spacing: 2px;
+    }
+
+    h2 {
+        font-weight: 700;
+        font-size: 2.5rem;
+        line-height: 2.75rem;
+        letter-spacing: 1.42857px;
+    }
+
+    h3 {
+        font-weight: 700;
+        font-size: 2rem;
+        line-height: 2.25rem;
+        letter-spacing: 1.14286px;
+    }
+
+    h4 {
+        font-weight: 600;
+        font-size: 1.75rem;
+        line-height: 2.375rem;
+        letter-spacing: 2px;
+    }
+
+    h5 {
+        font-weight: 400;
+        font-size: 1.5rem;
+        line-height: 2.0625rem;
+        letter-spacing: 1.71429px;
+    }
+
+    h6 {
+        font-weight: 400;
+        font-size: 1.125rem;
+        line-height: 1.5625rem;
+        letter-spacing: 1.28571px;
+    }
+
+    p {
+        font-weight: 300;
+        font-size: 0.9375rem;
+        line-height: 1.5625rem;
+    }
+
     a {
         text-decoration: none ;
         cursor: pointer;
